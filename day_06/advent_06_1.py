70838f88dd4adc0f1db3368614c1b5279efa328f
Chris LaSota <lasota371@gmail.com>
2023-12-06 21:29:35 -0700

    added code solution to part 1

---
+++ day_06/advent_06_1.py
@@ -0,0 +1,28 @@
+# Advent of Code 2023
+# Day 6
+# Part 1
+
+import re
+
+def main() -> int:
+    ways_product = 1
+    with open("input_day_06.txt", 'r') as input_file:
+        # read in race times and record distances as arrays of stringified numbers
+        data_filter = re.compile('([0-9]+)')
+        race_times = data_filter.findall(input_file.readline())
+        record_distances = data_filter.findall(input_file.readline())
+
+    for race in range(len(race_times)):
+        max_time = int(race_times[race])
+        ways_to_win = 0
+        for t in range(max_time + 1):
+            distance = t*(max_time - t)
+            if distance > int(record_distances[race]):
+                ways_to_win += 1
+        ways_product *= ways_to_win
+
+    return ways_product
+
+
+if __name__ == "__main__":
+    print(f"The product of the number of ways to beat each record is : {main()}")
