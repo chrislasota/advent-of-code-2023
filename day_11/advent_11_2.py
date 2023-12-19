4d1f960c711b137cf338291436208be6d176be80
Chris LaSota <lasota371@gmail.com>
2023-12-19 05:32:51 -0700

    Fixed output message

--- day_11/advent_11_2.py
+++ day_11/advent_11_2.py
@@ -56,4 +56,4 @@ def main() -> int:
 
 
 if __name__ == "__main__":
-    print(f"The number of steps to the furthest location is : {main()}")
+    print(f"The sum of pairwise lengths between galaxies is : {main()}")
